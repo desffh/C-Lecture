1ed13ce12f17a6ddcfd7e749e7630d1cce4c9c5b
desffh <yangju058@gmail.com>
2024-03-28 19:50:01 +0900

    (2024.3.28) 재귀 함수

--- Program/Source.c
+++ Program/Source.c
@@ -1,96 +1,110 @@
 #include <stdio.h>
+//void Awake()
+//{
+//	printf("Awake\n");
+//
+//	Awake();
+//}
 
-void Function()
+void Recursive(int count)
 {
-	printf("Function\n");
-}
+	if (count >= 4)
+	{
+		return;
+	}
+	printf("count 변수의 값:%d\n", count);
 
-void Damage(int attack)
+	Recursive(count + 1);
+}
+inline void Process(int data)
 {
-	printf("%d만큼 Damage를 받았습니다.\n", attack);
-}
+	printf("%d%%만큼 데이터가 처리되었습니다.", data);
 
-float Add(float x, float y)
-{
-	return x + y;
+	// 인라인 함수는 컴파일 시점에 확장되며, 컴파일 시
+	// 온라인 함수로 선언하더라도 상황에 따라 일반 함수로
+	// 변환되기도 합니다.
 }
-
-void Recovery(float health)
-{
-	health = 100;
-}
-
-void Swap(int *x, int *y)
-{
-	int z = *y;
-	*y = *x;
-	*x = z;
-}
-
 
 int main()
 {
-#pragma region 함수
-	// 하나의 특별한 목적의 작업을 수행하기 위해
-	// 독립적으로 설계된 코드의 집합입니다.
+#pragma region 재귀 함수
+	// 어떤 함수에서 자신을 다시 호출하여 작업을
+	// 수행하는 함수입니다. 
+	//	Awake();
+	// 재귀 함수는 함수를 계속 호출하기 때문에 
+	// 스택 영역에 메모리가 계속 쌓이게 되므로
+	// 스택 오버플로우가 발생합니다.
+	//Recursive(1);
 
-	//Function();
+	// 재귀 함수의 경우 특정한 시점에서 함수를
+	// 반환해야 하며, 재귀적으로 호출한 함수는
+	// 스택 프레임에 의해 마지막에 호출된 함수
+	// 부터 차례대로 스택 영역에서 해제됩니다.
 
-	// 함수의 경우 자료형과 반환하는 값의 형태가
-	// 일치하지 않으면 원하는 값을 얻을 수 없습니다.
+#pragma endregion
+
+#pragma region 최댓값과 최솟값
+//int count[] = { 6,11,1,2,8 };
+//int max = count[0];
+//int min = count[0];
+//for (int i = 1; i < 5; i++)
+//{
+//	if (count[i] > max)
+//	{
+//		max = count[i];
+//	}
+//	if (count[i] < min)
+//	{
+//		min = count[i];
+//	}
+//}
+//printf("최대값: %d\n", max);
+//printf("최솟값: %d", min);
 
 
 
 #pragma endregion
 
-#pragma region 매개 변수
-	// 함수의 정의에서 전달받은 인수를 함수 내부로 
-	// 전달하기 위해 사용하는 변수입니다.
+#pragma region 인라인 함수
+	// 함수를 호출하는 대신 함수가 호출되는 위치마다
+	// 함수의 코드를 복사하여 전달하는 방식의 함수 입니다.
 
-	//Damage(25);
+	//Process(75);
 
-	// 매개 변수는 함수 내부에서만 연산이 이루어지며,
-	// 함수가 종료되면 메모리에서 사라지는 특징을 가지고 있습니다.
-
-	//printf("Add(10.5f, 20.75f)의 결과: %f\n", Add(10.5f, 20.75f));
-
-	// 하나의 함수에서 서로 다른 자료형의 매개 변수를 함께 정의할
-	// 수 있으며, 여러 개의 매개 변수를 선언할 수도 있습니다.
+	// 인라인 함수는 함수를 호출하는 과정이 없으므로
+	// 처리 속도가 빠르지만, 인라인 함수를 많이 사용하게 되면
+	// 함수의 코드가 복사되기 때문에 실행 파일의
+	// 크기가 커지게 됩니다. (함수위치로 들어가는게 아니라 통째로 복사해서 
+	// main함수로 가져오기 때문에 파일 크기 커짐)
 
 #pragma endregion
 
-#pragma region 인수
-	// 함수가 호출될 때 매개 변수의 실제로
-	// 전달되는 값입니다.
-	
-	//int health = 50;
-	//
-	//Recovery(health);
-	//printf("health 변수의 값: %d\n", health); //50
+#pragma region 소수
+	printf("숫자입력:");
 
-	// 함수를 호출할 때 변수의 값을 인수로 전달하게 
-	// 되면 함수의 매개 변수에 변수의 복사된 값이
-	// 전달되기 때문에 인수로 전달한 변수는 함수 내에서
-	// 일어난 연산에 대해 영향을 받지 않습니다.
-	
-	//int a = 10;
-	//int b = 20;
-	//
-	//printf("Swap 전 a 변수의 값: %d\n", a);
-	//printf("Swap 전 b 변수의 값: %d\n", b);
-	//
-	//Swap(&a, &b);
-	//
-	//printf("Swap 후 a 변수의 값: %d\n", a);
-	//printf("Swap 후 b 변수의 값: %d\n", b);
+	int count = 0;
+	scanf_s("%d", &count);
+	int flag = 0;
 
-	// 함수를 호출할 때 변수의 주소를 인수로 전달하게 되면
-	// 전달한 변수의 주소가 참조되어 인수로 전달한 변수의
-	// 값이 함수 내에서 일어난 연산에 대해 영향을 받습니다.
+	for (int i = 2; i <= count; i++)
+	{
+		if (count % i == 0);
+		{
+			flag++;
+		}
+		
+	}
+	if (flag == 1)
+	{	
+		printf("Prime Number");
+	}
+	else
+	{
+		printf("Not Prime Number");
+	}
 
 #pragma endregion
+
 
 	return 0;
-
-
 }
